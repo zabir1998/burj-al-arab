69e24edc972bbb14b7ac257b5386cff50555f0a5
zabir1998 <zabirahmed1998@gmail.com>
2020-09-13 01:13:41 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApAGWUoPxRqbvA_XntzTkOd3KsreFE53A",
+    authDomain: "burj-al-arab-3c5f9.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-3c5f9.firebaseio.com",
+    projectId: "burj-al-arab-3c5f9",
+    storageBucket: "burj-al-arab-3c5f9.appspot.com",
+    messagingSenderId: "537534401548",
+    appId: "1:537534401548:web:8be2a3b3a36d67c6147229"
+  };
+  export default firebaseConfig;
